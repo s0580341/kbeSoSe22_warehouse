518f265b66e3cbff59804f17913b605ab3104398
s0580341git config --global user.name s0580341git config --list --show-origin$ git config --list --show-origingit config --global user.name s0580341 <Aleks.Hinze@htwb.onmicrosoft.com>
2022-04-23 14:44:37 +0200

    Initial commit maven

---
+++ src/main/java/com/example/kbesose22/KbeSoSe22Application.java
@@ -0,0 +1,13 @@
+package com.example.kbesose22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeSoSe22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbeSoSe22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kbesose22/KbeSoSe22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kbesose22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeSoSe22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
